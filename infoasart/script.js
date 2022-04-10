f62cb295e6ff063a5b137f8f91aca87255089974
Zgoly <46041044+Zgoly@users.noreply.github.com>
2022-04-10 22:23:02 +0500

    Update script.js

--- infoasart/script.js
+++ infoasart/script.js
@@ -1,4 +1,6 @@
 $(document).on('scroll', function(){
+	if (window.scrollY > 100) $('.navbar').addClass('show')
+	else $('.navbar').removeClass('show')
 	$('.header-img').css("bottom", Math.max(window.scrollY / 3) + "px");
 	$('.center-all').css("transform", "translateY(" + -Math.max(window.scrollY * .05) + "px)");
 })

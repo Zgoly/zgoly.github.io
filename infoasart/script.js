16845193c782da2fedc194fc37847d9bc7a41363
Zgoly <46041044+Zgoly@users.noreply.github.com>
2022-07-03 23:47:43 +0500

    Delete infoasart directory

--- infoasart/script.js
+++
@@ -1,6 +0,0 @@
-$(document).on('scroll', function(){
-	if (window.scrollY > 10) $('.navbar').addClass('show')
-	else $('.navbar').removeClass('show')
-	$('.header-img').css("bottom", Math.max(window.scrollY / 3) + "px");
-	$('.center-all').css("transform", "translateY(" + -Math.max(window.scrollY * .05) + "px)");
-})
